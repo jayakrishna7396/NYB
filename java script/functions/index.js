cc53a5870941ce66545ee6643b68b47fb09c8941
jayakrishna <jaya.krishna@nybinfotech.com>
2026-06-10 17:53:34 +0530

    added func

--- java script/functions/index.js
+++ java script/functions/index.js
@@ -11,23 +11,23 @@ function add(a,b){   //parameters
 }
 add(1,2)          //only console is didnt  is get 3 return     //arugument
 //
-function add(a,b){
-    console.log(a+b)
-}
-console.log (add(1,2))   //undefined is getting undefined is coming because 
+// function add(a,b){
+//     console.log(a+b)
+// }
+// console.log (add(1,2))   //undefined is getting undefined is coming because 
+// //
+// function add(a,b){
+//     console.log(a+b)
+//     return a+b
+// }
+// console.log(add(1,2))
 //
-function add(a,b){
-    console.log(a+b)
-    return a+b
-}
-console.log(add(1,2))
-//
-function add(a,b){
-    console.log(a+b)
-    return"hello"
-}
-let result=add(1,2)
-console.log(result)
+// function add(a,b){
+//     console.log(a+b)
+//     return"hello"
+// }
+// let result=add(1,2)
+// console.log(result)
 
 
 //email
@@ -118,8 +118,11 @@ let c=()=> a+b
 
 
                   //var      //let       //const
-  //redeclare       yes       no             no       (redeclaring the same variable)
-//redeclion
+ //redeclare       yes       no             no       (redeclaring the same variable)
+ //reassigment       yes       yes            no
+ //hoisting         yes        no             no
+
+
 var m=10
 console.log(m)
 
@@ -135,3 +138,117 @@ console.log(m)
 //it causes a referenceerror
 //program stops with an error
 //  console.log(y)
+
+//var
+var age=10
+console.log (age)
+
+var age=30
+console.log(age)
+
+//let
+
+let k=10
+console.log(k)
+
+// let k=20     //Identifier 'k' has already
+
+// const
+// const 1=10
+// console.log(1)
+
+// const 1=20     //Identifier 'k' has already 
+// console.log(1)
+
+
+//scope
+
+var d=10              //result--1,2,10
+function hi(a,b){
+    console.log(a,b,d)
+}
+hi(1,2)
+
+// function hi(a,b,d){       //result--1,2,undefined
+//     console.log(a,b,d)
+// }
+// hi(1,2)
+
+// function hi(a,b,c,d){
+//     var m=10
+//     console.log(m)
+//     console.log(a,b,d)
+// }
+// hi(1,2)
+// console.log(m)
+
+
+
+//block  //{}inside and out side the block we can used var but let inside the block we can use otside the block cannot use
+// {
+//     var J=10
+//     let K=20
+//     const za=30
+//     console.log(za)
+//     console.log(K)
+//     console.log(J)
+// }
+// console.log(J)
+// console.log(K)
+// console.log(za)
+
+//variables is decleared in outside function
+//variables is declared inside the function
+//let and const are block scoope
+
+//function var,let,const
+//var---this can be accesed out the fumction
+// let,const---cannot be accesed
+
+//block--var,let and const
+//var---can be accesed outside the block
+//let,const---cannot be accesed out side the block
+
+
+//recursive function
+let sum=0
+let one=function rec(a){
+    if(a==0){
+        return  0
+    }
+    // sum+=a
+    return a+rec(a-1)
+}
+// return(10 +9+ 8+)
+//a=10 sum=10--->sum=10 a=9 sum=19....a=1 0 -1 -2 -3
+//sum of first 10 natural number
+console.log(one(9))
+console.log(one(10))
+
+//iife(immediately invoked function expression)
+
+
+
+// function functionname(a,b,c,d){
+
+// }
+// functionname(10,20,30)
+
+// function functionname(a){
+
+//     function(){
+//         function(){
+//             function(){
+
+//             }
+//         }
+//     }
+// }
+// functionname(10,20,30,40)
+
+function add(a){
+    return function(){
+        console.log("second function")
+    }
+}    
+console.log(add(10))
